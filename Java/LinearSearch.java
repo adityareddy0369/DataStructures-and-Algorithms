ebdf686b9462cc9a409a4acf1898188524bfe470
adityareddy0369 <35109157+adityareddy0369@users.noreply.github.com>
2021-05-30 23:09:07 +0530

    Create LinearSearch.java

---
+++ Java/LinearSearch.java
@@ -0,0 +1,22 @@
+package searching;
+
+public class LinearSearch {
+	public static void main(String[] args) {
+		int[] arr = {3, 5, 12, 43, 1, 14};
+		int size = arr.length;
+		int value = 3;
+		LinearSearch ls = new LinearSearch();
+		System.out.println(ls.findIndex(arr, size, value));
+	}
+	
+	public int findIndex(int[] arr, int size, int value) {
+		for(int i=0; i < size; i++) {
+			if(arr[i] == value) {
+				return i;
+			}
+		}
+		return -1;
+		
+	}
+
+}
